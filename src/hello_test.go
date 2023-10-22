879eecd35f27577b85fd72d8463972a8923a250d
K-taiga <taigakatou0329@gmail.com>
2023-10-22 12:53:47 +0900

    [update] hello worldのリファクタ

--- src/hello_test.go
+++ src/hello_test.go
@@ -3,11 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		// fは、プレースホルダー値％qに値が挿入された文字列を作成できる形式を表す
-		t.Errorf("got %q want %q", got, want)
+	// 関数内で関数を宣言し変数に割り当てられる *testing.T と *testing.B の両方が満たすインターフェース
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// このメソッドがヘルパーであるとテストスイートに伝えるもの
+		t.Helper()
+		if got != want {
+			// fは、プレースホルダー値％qに値が挿入された文字列を作成できる形式を表す
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
