e6de54f550d36654a78fedf83a0cc1015dd65060
K-taiga <taigakatou0329@gmail.com>
2023-10-22 15:18:56 +0900

    [add] finish hello world

--- src/hello.go
+++ src/hello.go
@@ -3,14 +3,34 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bondule, "
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
-    if name == "" {
-        name = "World"
-    }
-    return englishHelloPrefix + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+    return greetingPrefix(language) + name
 }
 
-func main()  {
-	fmt.Println(Hello("World"))
+func greetingPrefix(language string) (prefix string){
+    prefix := englishHelloPrefix
+
+    switch language {
+    case french:
+        prefix = frenchHelloPrefix
+    case spanish:
+        prefix = spanishHelloPrefix
+    default:
+        prefix = englishHelloPrefix
+    }
+    // 返り値にprefixが設定されているのでreturnで返せる
+	return
+}
+
+func main() {
+	fmt.Println(Hello("", ""))
 }
